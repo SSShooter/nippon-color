007a86c4d0081ad9972734831b7e2ea1aa0c4ae2
zhoudejie <ssshooterx@gmail.com>
2018-09-13 19:22:35 +0800

    optimize webpack config compress img in prod mode

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+var ImageminPlugin = require('imagemin-webpack-plugin').default
+
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production' ? '/nippon-color/' : '/',
   pwa: {
@@ -11,9 +13,32 @@ module.exports = {
       errors: true,
     },
   },
+  configureWebpack: {
+    plugins: [
+      new ImageminPlugin({
+        disable: process.env.NODE_ENV !== 'production', // Disable during development
+        pngquant: {
+          quality: '95-100',
+        },
+      }),
+    ],
+  },
   chainWebpack: config => {
-    config.resolve
-      .symlinks(true)
+    config.resolve.symlinks(true)
+    config.plugin('preload').tap(options => {
+      options[0] = {
+        rel: 'preload',
+        as (entry) {
+          if (/\.css$/.test(entry)) return 'style'
+          if (/\.(woff||ttf)$/.test(entry)) return 'font'
+          if (/\.png$/.test(entry)) return 'image'
+          return 'script'
+        },
+        include: 'allAssets',
+        fileBlacklist: [ /\.map$/, /hot-update\.js$/ ],
+      }
+      return options
+    })
     return config
   },
 }
